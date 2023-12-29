d427374d0123ed85d6f743d62f3b91662a109b1b
Sanjeev <sk3616593@gmail.com>
2023-12-30 00:56:53 +0530

    All component Base and fire base is completed or Done

---
+++ src/api/AuthAPI.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const LoginAPI = () => {};
+
+export default LoginAPI;
---
+++ src/components/common/LoginComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import LoginAPI from '../../api/AuthAPI'
+import '../../Sassgit init/LoginComponent.scss'
+const LoginComponent = () => {
+  return (
+    <div>
+      <h1>Login Component</h1>
+      <button>Log In Linkedin</button>
+    </div>
+  )
+}
+
+export default LoginComponent
